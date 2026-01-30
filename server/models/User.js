2d5ae59d2f25a339b66cb08509ca43781d8db496
Mariedel Apaitan <madie@khazu.net>
2026-01-30 10:36:43 +0800

    Backend for login

--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,6 @@ const userSchema = new mongoose.Schema({
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true }
-});
+}, { collection: 'users' });
 
 module.exports = mongoose.model('User', userSchema);
